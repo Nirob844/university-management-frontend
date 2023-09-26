09481b2491ab38f69cf5087f0cbc1ae26751340b
Nirob Hasan <nirob427417@gmail.com>
2023-09-26 19:46:26 +0600

    login from

--- src/components/ui/Contents.tsx
+++ src/components/ui/Contents.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { Layout } from "antd";
+import UmBreadCrumb from "./UmBreadCrumb";
 
 const { Content } = Layout;
+const base = "admin";
 
 const Contents = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -11,6 +13,18 @@ const Contents = ({ children }: { children: React.ReactNode }) => {
         color: "black",
       }}
     >
+      <UmBreadCrumb
+        items={[
+          {
+            label: `${base}`,
+            link: `/${base}`,
+          },
+          {
+            label: "student",
+            link: `/${base}/student`,
+          },
+        ]}
+      />
       {children}
     </Content>
   );
